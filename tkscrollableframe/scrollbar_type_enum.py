65eb7296c42571bbedca97357ec7d4198c7d39bb
Ed <edfillingham1@gmail.com>
2024-03-27 16:44:11 +0000

    Fixed missing docstring

--- tkscrollableframe/scrollbar_type_enum.py
+++ tkscrollableframe/scrollbar_type_enum.py
@@ -7,6 +7,9 @@ from enum import Enum
 
 
 class ScrollbarsType(Enum):
+    """
+    An enum class that defines the types of scrollbars that can be used
+    """
     VERTICAL = "vertical"  # If you only want a vertical scrollbar
     HORIZONTAL = "horizontal"  # If you only want a horizontal scrollbar
     BOTH = "both"  # If you want both a vertical and horizontal scrollbar
